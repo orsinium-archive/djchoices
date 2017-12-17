20fcdae49d9958a2413fe3fffc9cecb55c12d3f5
orsinium <master_fess@mail.ru>
2017-12-17 12:31:52 +0500

    +new methods

--- djchoices/__init__.py
+++ djchoices/__init__.py
@@ -13,3 +13,21 @@ class Choices(Enum):
     def choices(cls):
         choices = cls.__members__.values()
         return tuple([(v.db, v.verbose) for v in choices])
+
+    @classmethod
+    def by_db(cls, db):
+        members = {member.db: member for member in cls.__members__.values()}
+        return members[db]
+
+    @classmethod
+    def by_verbose(cls, verbose):
+        members = {member.verbose: member for member in cls.__members__.values()}
+        return members[verbose]
+
+    @classmethod
+    def verbose_by_db(cls, db):
+        return cls.by_db(db).verbose
+
+    @classmethod
+    def db_by_verbose(cls, verbose):
+        return cls.by_verbose(verbose).db
